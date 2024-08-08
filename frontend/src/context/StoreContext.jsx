71432f8f18d2f25f95645eae6bb1e532189ea1b4
Bao Huy <diepbaohuy25112003@gmail.com>
2024-08-08 21:02:28 +0700

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://fur-del-backend.onrender.com";
   const [token, setToken] = useState("");
   const [furniture_list, setFurnitureList] = useState([]);
 
